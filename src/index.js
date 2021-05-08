99ba0ad7c60db07b6cdbc4721cee5d2c71121571
Katherine Gomez <katherinegomez.mfs@gmail.com>
2021-05-08 12:36:56 -0400

    frontend complete

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//Import router
+import {BrowserRouter as Router} from "react-router-dom"
 
+//Wrap our application inside of router to enable using router
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
